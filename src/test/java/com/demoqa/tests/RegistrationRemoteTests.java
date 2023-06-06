cd6b10e2b512a7c890c14a8e9418892da16c2dee
dboyko-qa <darya.boyko@gmail.com>
2023-06-06 12:30:14 +0300

    added steps, attachments for Allure

--- src/test/java/com/demoqa/tests/RegistrationRemoteTests.java
+++ src/test/java/com/demoqa/tests/RegistrationRemoteTests.java
@@ -4,6 +4,8 @@ import com.demoqa.pages.RegistrationPage;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
 public class RegistrationRemoteTests extends RemoteTestBase {
 
     @Test
@@ -13,20 +15,23 @@ public class RegistrationRemoteTests extends RemoteTestBase {
         Student student = new Student();
         RegistrationPage registrationPage = new RegistrationPage();
 
-        registrationPage.openPage()
-                .setFirstName(student.firstName)
-                .setLastName(student.lastName)
-                .setUserEmail(student.userEmail)
-                .setUserNumber(student.userNumber)
-                .setSubject(student.subject)
-                .setGender(student.gender)
-                .setBirthDate(student.dateOfBirthDay, student.dateOfBirthMonth, student.dateOfBirthYear)
-                .setHobby(student.hobby)
-                .uploadPicture(student.imgFileName)
-                .setCurrentAddress(student.currentAddress)
-                .setState(student.state)
-                .setCity(student.city)
-                .submit();
+        step("Fill form", () ->
+                registrationPage.openPage()
+                        .setFirstName(student.firstName)
+                        .setLastName(student.lastName)
+                        .setUserEmail(student.userEmail)
+                        .setUserNumber(student.userNumber)
+                        .setSubject(student.subject)
+                        .setGender(student.gender)
+                        .setBirthDate(student.dateOfBirthDay, student.dateOfBirthMonth, student.dateOfBirthYear)
+                        .setHobby(student.hobby)
+                        .uploadPicture(student.imgFileName)
+                        .setCurrentAddress(student.currentAddress)
+                        .setState(student.state)
+                        .setCity(student.city));
+        step("Submit", () -> registrationPage.submit());
+
+        step("Verify result", () ->
         registrationPage.verifyModalAppears()
                 .verifyResult("Student Name", student.firstName + " " + student.lastName)
                 .verifyResult("Student Email", student.userEmail)
@@ -37,7 +42,8 @@ public class RegistrationRemoteTests extends RemoteTestBase {
                 .verifyResult("Hobbies", student.hobby)
                 .verifyResult("Picture", student.imgFileName)
                 .verifyResult("Address", student.currentAddress)
-                .verifyResult("State and City", student.state + " " + student.city);
+                .verifyResult("State and City", student.state + " " + student.city)
+        );
 
 
     }
--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -1,8 +1,13 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import com.demoqa.helpers.Attach;
 
 import java.util.Map;
 
@@ -24,5 +29,18 @@ public class RemoteTestBase {
         ));
 
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
